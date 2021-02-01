3f1dda2b9c2b04ec9f861c56fb8fd437b74659b5
turinglabsorg <s.cataudo@turinglabs.org>
2021-02-01 12:56:33 +0100

    linting

--- shot.js
+++ shot.js
@@ -5,44 +5,46 @@ var argv = require('minimist')(process.argv.slice(2));
 
 const run = async () => {
   if (argv.t !== undefined) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    console.log('Setting up viewport...')
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    console.log('Setting up viewport...');
+    
     await page.setViewport({
       width: 800,
       height: 1200
-    })
+    });
 
-    await page.goto(argv.t)
-    const split = argv.t.split('/')
+    await page.goto(argv.t);
+    const split = argv.t.split('/');
+    const filename = './shots/' + split[3] + '/' + split[5] + '.png';
 
     if (!fs.existsSync('./shots/' + split[3])) {
-      fs.mkdirSync('./shots/' + split[3])
+      fs.mkdirSync('./shots/' + split[3]);
     }
-    const filename = './shots/' + split[3] + '/' + split[5] + '.png'
-
-    console.log('Loading tweet #' + split[5] + ' from @' + split[3] + '...')
+    
+    console.log('Loading tweet #' + split[5] + ' from @' + split[3] + '...');
 
     setTimeout(async function () {
-      let element = await page.$('article')
-      let coordinates = await element.boundingBox()
+      let element = await page.$('article');
+      let coordinates = await element.boundingBox();
 
       await page.screenshot({
         path: filename,
         fullPage: false
-      })
-
+      });
 
       await browser.close();
-      let buf = fs.readFileSync(filename)
-      let maxh = parseInt(coordinates.height.toFixed(0)) - 50
+
+      let buf = fs.readFileSync(filename);
+      let maxh = parseInt(coordinates.height.toFixed(0)) - 50;
+
       sharp(buf).extract({ left: 130, top: 55, width: 590, height: maxh })
         .toFile(filename, (err, info) => {
-          console.log('Tweet picture created successfully at ' + filename)
+          console.log('Tweet picture created successfully at ' + filename);
         });
-    }, 5000)
+    }, 5000);
   } else {
-    console.log('PROVIDE TWEET URL WITH -t PARAM LIKE: node shot -t=https://twitter.com/turinglabsorg/status/1355191990728749057')
+    console.log('PROVIDE TWEET URL WITH -t PARAM LIKE: node shot -t=https://twitter.com/turinglabsorg/status/1355191990728749057');
   }
 };
 
